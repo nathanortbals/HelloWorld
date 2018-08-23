156e25f849629c972592948ce9a4d0e43c82dd83
Nathan Ortbals <nathan.ortbals@gmail.coma>
2018-08-23 12:25:29 -0500

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Nathan Ortbals on 8/23/18.
+//  Copyright © 2018 Nathan Ortbals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
